9ea7ee1e424e66a9877a1a3e0da5914aae16472a
Yunus Yeşilçimen <135322317+esbyunus@users.noreply.github.com>
2024-02-25 22:54:12 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿namespace askerlikswitch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Okuyor musunuz? (evet/hayir) ");
+            string okul = Console.ReadLine().ToLower();
+
+            switch(okul)
+            {
+                case "evet":
+                    Console.WriteLine("Askerlige gidemezsiniz");
+                    break;
+                case "hayir":
+                    Console.WriteLine("kac yasindasiniz? ");
+                    int yas = Convert.ToInt32(Console.ReadLine());
+                    if (yas >= 18)
+                    {
+                        Console.WriteLine("Askerlige gidebilirsiniz.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Askerlige gidemezsiniz");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Hatali yazim");
+                    break;
+
+
+
+
+            }
+                
+
+
+        }
+    }
+}
